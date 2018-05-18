9d106d304b5601ffed25af233e53d430ecd30dc7
Kyle Morton <kylemortondevelopment@gmail.com>
2018-05-18 13:03:10 -0500

    create blog entry model, updated readme with position in tutorial

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 ### Start MongoDB
 "C:\Program Files\MongoDB\Server\3.6\bin\mongod.exe" --dbpath C:\Mongodb\data
+
+https://www.codementor.io/olatundegaruba/nodejs-restful-apis-in-10-minutes-q0sgsfhbd#setting-up-the-routes
---
+++ api/models/blogEntryModel.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var BlogEntrySchema = new Schema({
+    content: {
+        type: String,
+        required: 'please enter the post body'
+    },
+    title: {
+        type: String,
+        required: 'please enter the post title'
+    },
+    Created_date: {
+        type: Date,
+        default: Date.now
+    },
+    status: {
+        type: [{
+          type: String,
+          enum: ['pending', 'active', 'archived']
+        }],
+        default: ['pending']
+    }
+});
+
+module.exports = mongoose.model('BlogEntries', BlogEntrySchema);
