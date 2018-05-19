5162555b69bb5ea9f0f04cc512bcd49ce6dda9a0
Kyle Morton <kylemortondevelopment@gmail.com>
2018-05-19 18:33:56 -0500

    changed connection to hit mongoDB online database

--- server.js
+++ server.js
@@ -7,12 +7,17 @@ var express = require('express'),
   
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/Blogdb'); 
+// mongoose.connect('mongodb://localhost/Blogdb'); 
+mongoose.connect('mongodb+srv://blog_rw_user:password1234@kmnosql-ln6px.mongodb.net/BlogDb');
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+//on incorrect route -> send 404 - not found message
+// app.use(function(req, res) {
+//   res.status(404).send({url: req.originalUrl + ' not found'})
+// });
 
 var routes = require('./api/routes/blogRoutes'); //importing route
 routes(app); //register the route
